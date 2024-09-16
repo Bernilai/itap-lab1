e6277080f7acf0a2768aecb476d94331835a05fc
Bernilai <ilyabolotov2803@gmail.com>
2024-09-16 21:17:22 +0300

    Initial commit

---
+++ src/main/java/dev/Palindrome.java
@@ -0,0 +1,28 @@
+package dev;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a word: ");
+        String word = sc.nextLine();
+
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is not a palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(String word) {
+
+        StringBuilder reversed_word = new StringBuilder(word);
+        reversed_word.reverse();
+
+        return word.contentEquals(reversed_word);
+    }
+}
